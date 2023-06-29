448c7b13e107d04edc577c8f2478ff1cb1c5e101
naani <nandi.zalka@gmail.com>
2023-06-29 20:43:07 +0200

    Refactor mine counting

--- main.py
+++ main.py
@@ -11,82 +11,38 @@ map_details = {
 
 # also 13*15 with 40 mines, 16*30 with 99 mines
 
+
+def get_random_coords(rows, columns, map):
+        row_index = randint(0, rows - 1)
+        column_index = randint(0, columns - 1)
+        if map[row_index][column_index] != -1:
+            return [row_index, column_index]
+        else:
+            return get_random_coords(rows, columns, map)
+
 def crate_field(rows, columns, mines, map):
     map = [[0 for _ in range(columns)] for _ in range(rows)]
     for i in range(mines):
-        row_index = randint(0, rows - 1)
-        column_index = randint(0, columns - 1)
+        [row_index, column_index] = get_random_coords(rows, columns, map)
         map[row_index][column_index] = -1
+        count_mines(map, row_index, column_index)
 
     return map
 
-
-def count_mines(map):
-    for x in range(len(map)):
-        for y in range(len(map[x])):
-            if map[x][y] != -1:
-                try:
-                    if map[x-1][y-1] == -1 and y > 0 and x > 0:
-                        map[x][y] += 1
-
-                except IndexError:
-                    print('Out of bounds')
-
-                try:
-                    if map[x][y-1] == -1 and y > 0:
-                        map[x][y] += 1
-
-                except IndexError:
-                    print('Out of bounds')
-
-                try:
-                    if map[x+1][y-1] == -1 and y > 0:
-                        map[x][y] += 1
-
-                except IndexError:
-                    print('Out of bounds')
-                
-                try:
-                    if map[x-1][y] == -1 and x > 0:
-                        map[x][y] += 1
-
-                except IndexError:
-                    print('Out of bounds')
-                    
-                try:
-                    if map[x+1][y] == -1:
-                        map[x][y] += 1
-
-                except IndexError:
-                    print('Out of bounds')
-
-                try:
-                    if map[x-1][y+1] == -1 and x > 0:
-                        map[x][y] += 1
-
-                except IndexError:
-                    print('Out of bounds')
-                
-                try:
-                    if map[x][y+1] == -1:
-                        map[x][y] += 1
-
-                except IndexError:
-                    print('Out of bounds')
-
-                try:
-                    if map[x+1][y+1] == -1:
-                        map[x][y] += 1
-
-                except IndexError:
-                    print('Out of bounds')
+def count_mines(map, y, x):
+    if y-1 > -1 and map[y-1][x] != -1: map[y-1][x] += 1                                     #north
+    if x+1 < len(map[0]) and map[y][x+1] != -1: map[y][x+1] += 1                            #east
+    if y+1 < len(map) and map[y+1][x] != -1: map[y+1][x] += 1                               #south
+    if x-1 > -1 and map[y][x-1] != -1: map[y][x-1] += 1                                     #west
+    if y-1 > -1 and x+1 < len(map[0]) and map[y-1][x+1] != -1: map[y-1][x+1] += 1           #north-east    
+    if y+1 < len(map) and x+1 < len(map[0]) and map[y+1][x+1] != -1: map[y+1][x+1] += 1     #south-east
+    if y+1 < len(map) and x-1 > -1 and map[y+1][x-1] != -1: map[y+1][x-1] += 1              #south-west
+    if y-1 > -1 and x-1 > -1 and map[y-1][x-1] != -1: map[y-1][x-1] += 1                    #north-west
 
 def main():
-
     field = []
     field = crate_field(map_details["x"], map_details["y"], map_details["mines"], field)
     headers = ["A","B","C","D","E","F","G","H","I"]
-    count_mines(field)
     print(tabulate(field, headers, tablefmt="grid"))
 
 main()
