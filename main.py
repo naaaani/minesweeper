7c4bc7168aedb1baafb3e3928e5272bb9931975b
naani <nandi.zalka@gmail.com>
2023-07-10 14:28:27 +0200

    Add Display, Grid

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import pygame
 from random import randint
 from tabulate import tabulate
 
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 1000
+background_colour = (234, 212, 252)
+SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Minesweeper')
+SCREEN.fill(background_colour)
+
 map_details = {
     "x": 9,
     "y": 9,
@@ -10,7 +17,6 @@ map_details = {
 }
 
 # also 13*15 with 40 mines, 16*30 with 99 mines
-
 
 def get_random_coords(rows, columns, map):
         row_index = randint(0, rows - 1)
@@ -39,10 +45,28 @@ def count_mines(map, y, x):
     if y+1 < len(map) and x-1 > -1 and map[y+1][x-1] != -1: map[y+1][x-1] += 1              #south-west
     if y-1 > -1 and x-1 > -1 and map[y-1][x-1] != -1: map[y-1][x-1] += 1                    #north-west
 
+def drawGrid():
+    blockSize = 20
+    for x in range(0, WINDOW_WIDTH, blockSize):
+        for y in range(0, WINDOW_HEIGHT, blockSize):
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(SCREEN, (0, 0, 0), rect, 1)
+
 def main():
     field = []
     field = crate_field(map_details["x"], map_details["y"], map_details["mines"], field)
     headers = ["A","B","C","D","E","F","G","H","I"]
     print(tabulate(field, headers, tablefmt="grid"))
+
+    drawGrid()
+    pygame.display.flip()
+    running = True
+    while running:
+
+        for event in pygame.event.get():
+        
+            if event.type == pygame.QUIT:
+                running = False
+
 
 main()
