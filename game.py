b0229e1cd2c5ffac8d2c7bc19dc4ad626e8c07da
naani <nandi.zalka@gmail.com>
2023-07-12 22:30:56 +0200

    Add Setmine method

--- game.py
+++ game.py
@@ -6,10 +6,10 @@ from tabulate import tabulate
 SCREEN_WIDTH = 255
 SCREEN_HEIGHT = 255
 MARGIN = 5
-background_colour = (234, 212, 252)
-SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+BACKGROUND_COLOR = (234, 212, 252) 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Minesweeper')
-SCREEN.fill(background_colour)
+screen.fill(BACKGROUND_COLOR)
 
 MAP_DETAILS = {
     "x": 9,
@@ -20,30 +20,30 @@ MAP_DETAILS = {
 # also 13*15 with 40 mines, 16*30 with 99 mines
 
 
-class Tile:
+class Tile():
 
     num_value = 0
     hidden = True
+    def set_mine(self):
+        self.num_value = -1
 
 
-def get_random_coords(rows, columns, map):
-    row_index = randint(0, rows - 1)
-    column_index = randint(0, columns - 1)
-    if map[row_index][column_index] != -1:
+def get_random_coords(field, row_count, column_count):
+    row_index = randint(0, row_count - 1)
+    column_index = randint(0, column_count - 1)
+    if field[row_index][column_index] != -1:
         return [row_index, column_index]
     else:
-        return get_random_coords(rows, columns, map)
+        return get_random_coords(field, row_count, column_count)
 
+def crate_field(row_count, column_count, mine_count):
+    field = [[Tile() for _ in range(column_count)] for _ in range(row_count)]
+    for i in range(mine_count):
+        [row_index, column_index] = get_random_coords(field, row_count, column_count)
+        field[row_index][column_index].set_mine()
+        count_mines(field, row_index, column_index)
 
-def crate_field(rows, columns, mines, map):
-    map = [[Tile() for _ in range(columns)] for _ in range(rows)]
-    for i in range(mines):
-        [row_index, column_index] = get_random_coords(rows, columns, map)
-        map[row_index][column_index].num_value = -1
-        count_mines(map, row_index, column_index)
-
-    return map
-
+    return field
 
 def count_mines(map, y, x):
     if y-1 > -1 and map[y-1][x].num_value != -1:
@@ -63,8 +63,7 @@ def count_mines(map, y, x):
     if y-1 > -1 and x-1 > -1 and map[y-1][x-1].num_value != -1:
         map[y-1][x-1].num_value += 1  # north-west
 
-
-def drawGrid(field): # TODO separate this into create, draw
+def draw_grid(field): # TODO separate this into create, draw
     color = (255, 255, 255)
     blocksize = 20
     font = pygame.font.SysFont('arial', 20)
@@ -73,20 +72,24 @@ def drawGrid(field): # TODO separate this into create, draw
 
     for row in range(MAP_DETAILS["x"]):
         for column in range(MAP_DETAILS["y"]):
+            #TODO function to put stg at [x,y]
             content = str(field[row][column].num_value)
             text = font.render(content, True, (0, 0, 0))
-            rect = pygame.draw.rect(SCREEN,
-                                    color,
-                                    [(MARGIN + blocksize) * column + MARGIN,
-                                     (MARGIN + blocksize) * row + MARGIN,
-                                        blocksize,  # height
-                                        blocksize])  # width
-            SCREEN.blit(text, rect)
+            rect = pygame.draw.rect(
+                screen, 
+                color,                    
+                [
+                    (MARGIN + blocksize) * column + MARGIN,
+                    (MARGIN + blocksize) * row + MARGIN,
+                    blocksize,  # height
+                    blocksize   # width
+                ]
+            )
+            screen.blit(text, rect)
             sprite_map.append(rect)
 
     return sprite_map
     
-
 def is_clicked(tile, click_coords):
     low_value = tile[0]
     square_lenght = tile[2]
@@ -96,13 +99,11 @@ def is_clicked(tile, click_coords):
         return True
     return False
 
-
 def main():
     pygame.init()
 
-    field = []
     field = crate_field(
-        MAP_DETAILS["x"], MAP_DETAILS["y"], MAP_DETAILS["mines"], field)
+        MAP_DETAILS["x"], MAP_DETAILS["y"], MAP_DETAILS["mines"])
     headers = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
 
     num_map = []
@@ -114,9 +115,10 @@ def main():
         num_map.append(new_line)
 
     print(tabulate(num_map, headers, tablefmt="grid"))
-    sprite_map = drawGrid(field)
-
+    sprite_map = draw_grid(field) #TODO storing of coords not needed
+    print(sprite_map)
     pygame.display.flip()
+
     running = True
     while running:
         events = pygame.event.get()
@@ -126,9 +128,9 @@ def main():
                 running = False
             elif event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
-                print (pos)
+                print(pos)
                 for tile in sprite_map:
-                    if is_clicked(tile, pos):
+                    if is_clicked(tile, pos): #TODO find tile by pos
                         print('hit')
                         break
 
