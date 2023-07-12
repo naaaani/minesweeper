b0977851b814f25e103ad94fd6361e45214233dd
naani <nandi.zalka@gmail.com>
2023-07-12 14:29:47 +0200

    Refactor map to objects

--- game.py
+++ game.py
@@ -20,6 +20,12 @@ MAP_DETAILS = {
 # also 13*15 with 40 mines, 16*30 with 99 mines
 
 
+class Tile:
+
+    num_value = 0
+    hidden = True
+
+
 def get_random_coords(rows, columns, map):
     row_index = randint(0, rows - 1)
     column_index = randint(0, columns - 1)
@@ -30,32 +36,32 @@ def get_random_coords(rows, columns, map):
 
 
 def crate_field(rows, columns, mines, map):
-    map = [[0 for _ in range(columns)] for _ in range(rows)]
+    map = [[Tile() for _ in range(columns)] for _ in range(rows)]
     for i in range(mines):
         [row_index, column_index] = get_random_coords(rows, columns, map)
-        map[row_index][column_index] = -1
+        map[row_index][column_index].num_value = -1
         count_mines(map, row_index, column_index)
 
     return map
 
 
 def count_mines(map, y, x):
-    if y-1 > -1 and map[y-1][x] != -1:
-        map[y-1][x] += 1  # north
-    if x+1 < len(map[0]) and map[y][x+1] != -1:
-        map[y][x+1] += 1  # east
-    if y+1 < len(map) and map[y+1][x] != -1:
-        map[y+1][x] += 1  # south
-    if x-1 > -1 and map[y][x-1] != -1:
-        map[y][x-1] += 1  # west
-    if y-1 > -1 and x+1 < len(map[0]) and map[y-1][x+1] != -1:
-        map[y-1][x+1] += 1  # north-east
-    if y+1 < len(map) and x+1 < len(map[0]) and map[y+1][x+1] != -1:
-        map[y+1][x+1] += 1  # south-east
-    if y+1 < len(map) and x-1 > -1 and map[y+1][x-1] != -1:
-        map[y+1][x-1] += 1  # south-west
-    if y-1 > -1 and x-1 > -1 and map[y-1][x-1] != -1:
-        map[y-1][x-1] += 1  # north-west
+    if y-1 > -1 and map[y-1][x].num_value != -1:
+        map[y-1][x].num_value += 1  # north
+    if x+1 < len(map[0]) and map[y][x+1].num_value != -1:
+        map[y][x+1].num_value += 1  # east
+    if y+1 < len(map) and map[y+1][x].num_value != -1:
+        map[y+1][x].num_value += 1  # south
+    if x-1 > -1 and map[y][x-1].num_value != -1:
+        map[y][x-1].num_value += 1  # west
+    if y-1 > -1 and x+1 < len(map[0]) and map[y-1][x+1].num_value != -1:
+        map[y-1][x+1].num_value += 1  # north-east
+    if y+1 < len(map) and x+1 < len(map[0]) and map[y+1][x+1].num_value != -1:
+        map[y+1][x+1].num_value += 1  # south-east
+    if y+1 < len(map) and x-1 > -1 and map[y+1][x-1].num_value != -1:
+        map[y+1][x-1].num_value += 1  # south-west
+    if y-1 > -1 and x-1 > -1 and map[y-1][x-1].num_value != -1:
+        map[y-1][x-1].num_value += 1  # north-west
 
 
 def drawGrid(field):
@@ -65,7 +71,7 @@ def drawGrid(field):
 
     for row in range(MAP_DETAILS["x"]):
         for column in range(MAP_DETAILS["y"]):
-            content = str(field[row][column])
+            content = str(field[row][column].num_value)
             text = font.render(content, True, (0, 0, 0))
             rect = pygame.draw.rect(SCREEN,
                                     color,
@@ -83,8 +89,16 @@ def main():
     field = crate_field(
         MAP_DETAILS["x"], MAP_DETAILS["y"], MAP_DETAILS["mines"], field)
     headers = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
-    print(tabulate(field, headers, tablefmt="grid"))
 
+    num_map = []
+    
+    for row in field:
+        new_line = []
+        for tile in row:
+            new_line.append(tile.num_value)
+        num_map.append(new_line)
+
+    print(tabulate(num_map, headers, tablefmt="grid"))
     drawGrid(field)
 
     pygame.display.flip()
@@ -99,9 +113,6 @@ def main():
                 pos = pygame.mouse.get_pos()
                 column = pos[0] // (SCREEN_WIDTH + MARGIN)
                 row = pos[1] // (SCREEN_HEIGHT + MARGIN)
-                # field[row][column] = 1
-                print(pos)
-                print(row, column)
 
 
 main()
