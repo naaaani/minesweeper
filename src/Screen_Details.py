01f1bddbf28b2925162e9b83d12f693d7c70a993
naaaani <nandi.zalka@gmail.com>
2023-11-05 17:27:55 +0100

    Chage Screen_Details.font

--- src/Screen_Details.py
+++ src/Screen_Details.py
@@ -1,3 +1,5 @@
+import pygame
+
 class Screen_Details():
 
     def __init__(self, screen_width, screen_height, background_color, font):
@@ -6,7 +8,10 @@ class Screen_Details():
         self.menu_margin = self.screen_height / 20
         self.map_margin = self.screen_width / 20
         self.background_color = background_color
-        self.font = font
+        self.font = pygame.font.SysFont('arial', 20)
 
     def set_width(self, screen_width):
         self.screen_width = screen_width
+
+    def get_font(self):
+        return self.font
