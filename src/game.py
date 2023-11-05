b7c73e3630c137d8b632d7be35e8967fc028e120
naaaani <nandi.zalka@gmail.com>
2023-11-05 16:51:26 +0100

    Implement Detection of Clicked Tile

--- src/game.py
+++ src/game.py
@@ -5,32 +5,37 @@ from tabulate import tabulate
 from math import floor
 
 from Tile import Tile
-from Screen import Screen
+from Screen_Details import Screen_Details
 
 MAP_DETAILS = {
-    "x": 9,
-    "y": 9,
+    "x": 10,
+    "y": 10,
     "mines": 10,
 }
 
 # also 13*15 with 40 mines, 16*30 with 99 mines
 
-def get_random_coords(field, row_count, column_count):
+
+def get_random_coord(game_map, row_count, column_count):
     row_index = randint(0, row_count - 1)
     column_index = randint(0, column_count - 1)
-    if field[row_index][column_index] != -1:
+    if game_map[row_index][column_index] != -1:
         return [row_index, column_index]
     else:
-        return get_random_coords(field, row_count, column_count)
+        return get_random_coord(game_map, row_count, column_count)
+
 
 def crate_field(row_count, column_count, mine_count):
-    field = [[Tile(x, y) for x in range(column_count)] for y in range(row_count)]
-    for i in range(mine_count):
-        [row_index, column_index] = get_random_coords(field, row_count, column_count)
+    field = [[Tile(x, y) for x in range(column_count)]
+             for y in range(row_count)]
+    for _ in range(mine_count):
+        [row_index, column_index] = get_random_coord(
+            field, row_count, column_count)
         field[row_index][column_index].set_mine()
         count_mines(field, row_index, column_index)
 
     return field
+
 
 def count_mines(map, y, x):
     if y-1 > -1 and map[y-1][x].num_value != -1:
@@ -50,11 +55,12 @@ def count_mines(map, y, x):
     if y-1 > -1 and x-1 > -1 and map[y-1][x-1].num_value != -1:
         map[y-1][x-1].num_value += 1  # north-west
 
+
 def draw_table(field):
     headers = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
 
     num_map = []
-    
+
     for row in field:
         new_line = []
         for tile in row:
@@ -63,78 +69,62 @@ def draw_table(field):
 
     print(tabulate(num_map, headers, tablefmt="grid"))
 
-def draw_grid(field, display, screen):
-    color = (255, 255, 255)
-    block_height = floor(display.screen_height / MAP_DETAILS["y"])
-    block_width = block_height
-    font = pygame.font.SysFont('arial', 20)
-    side_margin = (display.screen_width - (display.map_margin * 2)  - (MAP_DETAILS["y"] * block_width)) / 2 
+
+def draw_grid(field, screen_details, screen_surface):
+    white = (255, 255, 255)
+    block_width = 20
+    block_height = block_width
+    margin = 5
 
     for row in range(MAP_DETAILS["x"]):
         for column in range(MAP_DETAILS["y"]):
-            #TODO function to put stg at [x,y]
-            content = str(field[row][column].num_value)
-            text = font.render(content, True, (0, 0, 0))
+            color = white
+            if field[row][column].is_mine():
+                color = (0, 0, 0)
+            if field[row][column].is_flagged():
+                color = (255, 0, 0)
+            pygame.draw.rect(screen_surface,
+                             color,
+                             pygame.Rect((margin + block_width) * column + margin,
+                                         (margin + block_height) * row + margin,
+                                         block_width,
+                                         block_height))
+    # TODO function to put stg at [x,y]
 
-            rect_left = (block_height) * column# + display.map_margin + side_margin
-            rect_top = (block_width) * row#  + display.map_margin + display.menu_margin
-
-            rect = pygame.draw.rect(
-                screen, 
-                color,
-                pygame.Rect(rect_left, rect_top, block_height, block_width),
-                5 # border width
-            )
-            if (row == 0):
-                print("top left: " + str(rect.topleft[0]), str(rect.topleft[1]) + " width: " + str(rect.width))
-                print("top right: " + str(rect.topright[0]), str(rect.topright[0]))
-            screen.blit(text, rect)
-    
-def is_clicked(tile, click_coords):
-    low_value = tile[0]
-    square_lenght = tile[2]
-    high_value = low_value + square_lenght
-
-    if click_coords[0] > low_value and click_coords[0] < high_value:
-        return True
-    return False
 
 def main():
     pygame.init()
-    display = Screen(600, 600, (234, 212, 252))
+    display = Screen_Details(255, 255, (0, 0, 0), font="Arial")
 
-    screen = pygame.display.set_mode((display.screen_width, display.screen_height))
+    screen = pygame.display.set_mode(
+        (display.screen_width, display.screen_height))
     pygame.display.set_caption('Minesweeper')
     screen.fill(display.background_color)
 
-
     field = crate_field(
         MAP_DETAILS["x"], MAP_DETAILS["y"], MAP_DETAILS["mines"])
-    
+
     draw_table(field)
 
     draw_grid(field, display, screen)
-    
-    pygame.display.flip()
 
-    block_width = (display.screen_height - display.menu_margin -
-                    display.map_margin) / MAP_DETAILS["y"]
-    side_margin = (display.screen_width - (display.map_margin * 2) -
-                    (MAP_DETAILS["y"] * block_width)) / 2
-    side_margin = (display.screen_width - (display.map_margin * 2) -
-                    (MAP_DETAILS["y"] * block_width)) / 2 
+    pygame.display.flip()
 
     running = True
     while running:
+        pygame.display.flip()
+        draw_grid(field, display, screen)
         events = pygame.event.get()
         for event in events:
 
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONUP:
-                mouse_pos = pygame.mouse.get_pos() #TODO calculate tile clicked
-                print(mouse_pos)
-                print(mouse_pos[0]/block_width)
-                print(floor((mouse_pos[0] / block_width)))
+                mouse_pos = pygame.mouse.get_pos()  # TODO calculate tile clicked
+                column = mouse_pos[0] // (20 + 5)
+                row = mouse_pos[1] // (20 + 5)
+                field[row][column].set_flag()
+                print(row, column)
+
 
 main()
