e0166194ee391ff0039f276259fc2e2d4f5f319c
naaaani <nandi.zalka@gmail.com>
2024-07-17 10:52:38 +0200

    Change game class to be the main file

---
+++ src/Game.py
@@ -0,0 +1,50 @@
+import pygame
+
+from menu_state import Menu
+from play_state import Play
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        
+        self.screen_width = 800
+        self.screen_height = 800
+        self.create_screen()
+        
+        self.menu = Menu(self)
+        self.play = Play(self)
+        
+        self.active_state = None
+        self.set_active_state(self.menu)
+
+    def create_screen(self):
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height,))
+        pygame.display.flip()
+    
+    def set_active_state(self, state):
+        if self.active_state is not None:
+            self.active_state.deactivate()
+        self.active_state = state
+        self.active_state.activate()
+    
+    def get_screen(self):
+        return self.screen
+    
+    def get_screen_dim(self):
+        return (self.screen_width, self.screen_height,)
+    
+    def main_loop(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit()
+                self.active_state.proc_event(event)
+                pygame.display.get_surface().fill((200,200,255,))
+                self.active_state.update()
+                pygame.display.update()     
+    
+
+if __name__ == "__main__":
+    game = Game()
+    game.main_loop()
